f14650684f67d3baa4aaa7d2ea9e77cff37f1acf
shapaterbhai <132383776+shapaterbhai@users.noreply.github.com>
2023-08-02 13:30:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      window.location.href = 'home.html'; // Replace 'index.html' with the actual home page URL
+    }, 5000); // 5 seconds
+  });
+  
+
